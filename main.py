780dc6abb6cd501057ebeb5e8abee425593746b2
nickcall2003 <126628426+nickcall2003@users.noreply.github.com>
2026-06-04 11:56:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-"""
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ROOT WORKING"}
 main.py
 -------
 Run:  uvicorn main:app --reload   (locally)
